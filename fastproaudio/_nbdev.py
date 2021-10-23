89fbd6a865b9862d2832c5ca67722a9cfab928ce
Scott H. Hawley <scott.hawley@belmont.edu>
2021-10-23 14:35:08 -0500

    added data download utils

--- fastproaudio/_nbdev.py
+++ fastproaudio/_nbdev.py
@@ -2,7 +2,11 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"say_hello": "00_core.ipynb"}
+index = {"URLs.AUDIOMDPI": "00_core.ipynb",
+         "URLs.MARCO": "00_core.ipynb",
+         "URLs.SIGNALTRAIN_LA2A_1_1": "00_core.ipynb",
+         "URLs.SIGNALTRAIN_LA2A_REDUCED": "00_core.ipynb",
+         "zenodo_url_to_data_url": "00_core.ipynb"}
 
 modules = ["core.py"]
 
